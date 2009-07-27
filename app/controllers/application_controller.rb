902f6b5eb5f8d282421a0475fc2f5bd0f9d7dc1c
James Healy <jimmy@deefa.com>
2009-07-27 12:51:36 +1000

    handle malformed csv errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
 
   include ExceptionNotifiable
 
+  rescue_from FasterCSV::MalformedCSVError, :with => :csv_error
+
+  # method for handling a malformed CSV file
+  def csv_error
+    render :template => 'errors/csv', :status => 500
+    true
+  end
+
 end
