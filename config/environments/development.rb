c08b976ea5cd37a517b7037ad20a71178ea1e1ca
James Healy <jimmy@deefa.com>
2011-04-05 10:36:29 +1000

    configure deprecation logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,4 +18,6 @@ CsvToOnix::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+
+  config.active_support.deprecation = :log
 end
